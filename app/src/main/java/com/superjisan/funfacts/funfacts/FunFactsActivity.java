715faedd2ff2289c9761997b6ec0335135058888
Jisan Zaman <jzthegreat@gmail.com>
2014-08-29 06:49:10 -0400

    random facts out of an array can now be displayed

--- app/src/main/java/com/superjisan/funfacts/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/superjisan/funfacts/funfacts/FunFactsActivity.java
@@ -1,9 +1,16 @@
 package com.superjisan.funfacts.funfacts;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import java.lang.reflect.Array;
+import java.util.Random;
 
 
 public class FunFactsActivity extends Activity {
@@ -12,6 +19,41 @@ public class FunFactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //Declare our view variables
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener(){
+          @Override
+            public void onClick(View view){
+              String[] facts = {
+                      "Ants stretch when they wake up in the morning.",
+                      "Ostriches can run faster than horses.",
+                      "Olympic gold medals are actually made mostly of silver.",
+                      "You are born with 300 bones; by the time you are an adult you will have 206.",
+                      "It takes about 8 minutes for light from the Sun to reach Earth.",
+                      "Some bamboo plants can grow almost a meter in just one day.",
+                      "The state of Florida is bigger than England.",
+                      "Some penguins can leap 2-3 meters out of the water.",
+                      "On average, it takes 66 days to form a new habit.",
+                      "Mammoths still walked the earth when the Great Pyramid was being built."
+              };
+
+              //The button was clicked, so update the quote with a new fact
+              String fact="";
+              //Randomly choose a fact
+              Random randomGenerator = new Random();
+              int randomNumber = randomGenerator.nextInt(facts.length);
+
+              fact = facts[randomNumber];
+
+              //update the fact with a dynamic fact
+              factLabel.setText(fact);
+          }
+        };
+
+        showFactButton.setOnClickListener(listener);
     }
 
 
